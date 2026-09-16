b82d3474dade1397e38de8f79f02315e63041e10
eduardofrancener <eduufrancener@gmail.com>
2026-09-16 14:28:05 -0300

    feat: apply AraquariDesk visible branding and default server

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,27 @@
 
 use librustdesk::*;
 
+fn configure_araquaridesk() {
+    *hbb_common::config::APP_NAME.write().unwrap() = "AraquariDesk".to_owned();
+
+    hbb_common::config::Config::set_option(
+        "custom-rendezvous-server".to_owned(),
+        "jiraiya.araquari.sc.gov.br".to_owned(),
+    );
+    hbb_common::config::Config::set_option(
+        "relay-server".to_owned(),
+        "jiraiya.araquari.sc.gov.br".to_owned(),
+    );
+    hbb_common::config::Config::set_option("api-server".to_owned(), "".to_owned());
+    hbb_common::config::Config::set_option(
+        "key".to_owned(),
+        "11CvwLZ0myJrVOg2amrOhqcKC0gZD1XIiFyL6QBnt+0=".to_owned(),
+    );
+}
+
 #[cfg(any(target_os = "android", target_os = "ios", feature = "flutter"))]
 fn main() {
+    configure_araquaridesk();
     if !common::global_init() {
         eprintln!("Global initialization failed.");
         return;
@@ -26,6 +45,7 @@ fn main() {
     unsafe {
         winapi::um::shellscalingapi::SetProcessDpiAwareness(2);
     }
+    configure_araquaridesk();
     if let Some(args) = crate::core_main::core_main().as_mut() {
         ui::start(args);
     }
